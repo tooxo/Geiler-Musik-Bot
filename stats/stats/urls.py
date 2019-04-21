b7c6cd76fba66a19c6479fc0a406a7a37ec06349
till <veganeslama@gmail.com>
2019-04-21 23:17:30 +0200

    Minor Changes.

--- stats/stats/urls.py
+++ stats/stats/urls.py
@@ -1,18 +1,3 @@
-"""stats URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -27,5 +12,5 @@ urlpatterns = [
     path('http/mongo_response', views.mongo_request_resp),
     path('http/mongo_most', views.mongo_request_most),
     path('response', views.resp),
-    path('mostplayed', views.mostplayed)
+    path('mostplayed', views.mostplayed),
 ]
