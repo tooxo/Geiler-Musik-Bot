e8ed9b4c718876a8e90e10d3f1ff9a989db5da64
tooxo <veganeslama@gmail.com>
2019-09-24 19:01:21 +0200

    Test Script Fixed

--- discord/run_tests.sh
+++ discord/run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-pip install distest
-cd discord || exit
+git clone https://github.com/tooxo/distest.git --depth 1
+cd distest
+pip install -r requirements-dev.txt
+pip install .
+cd ./../discord
 python3 discord_tests.py "$TARGET_NAME" "$TESTER_TOKEN" --channel "$CHANNEL" --run all
