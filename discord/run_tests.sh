f978c39e2479b795e926cd20e0a74ccc6b2e8660
tooxo <veganeslama@gmail.com>
2019-09-24 20:45:16 +0200

    Tests Fixed X (this time for real)

--- discord/run_tests.sh
+++ discord/run_tests.sh
@@ -6,6 +6,5 @@ cd distest
 pip3 install -r requirements-dev.txt
 pip3 install .
 cd ./../discord
-pip3 install -r requirements.txt
 pip3 install pynacl
 python3 -u discord_tests.py "$TARGET_NAME" "$TESTER_TOKEN" --channel "$CHANNEL" --run all
