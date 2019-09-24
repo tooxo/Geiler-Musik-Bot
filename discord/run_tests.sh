699f8f0a436630665482ee38ba95f377ec2c75df
tooxo <veganeslama@gmail.com>
2019-09-24 20:19:20 +0200

    Tests Fixed VII

--- discord/run_tests.sh
+++ discord/run_tests.sh
@@ -5,4 +5,5 @@ cd distest
 pip3 install -r requirements-dev.txt
 pip3 install .
 cd ./../discord
+pip3 install -r requirements.txt
 python3 -u discord_tests.py "$TARGET_NAME" "$TESTER_TOKEN" --channel "$CHANNEL" --run all
