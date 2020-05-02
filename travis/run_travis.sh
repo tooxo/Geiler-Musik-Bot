748c936c503bcecaef2e3959d47cb72419f051aa
till <till@s.chulte.de>
2020-05-03 01:11:40 +0200

    run unittests in docker

--- travis/run_travis.sh
+++ travis/run_travis.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-PYTHONWARNINGS="ignore" coverage run --source=.,src,node -m unittest discover -s src/
-PYTHONWARNINGS="ignore" coverage run --source=.,src,node -m unittest discover -s node/
+docker-compose build
+
+docker run --mount src=$(pwd)/node/,target=$(pwd)/node,type=bind -w="$(pwd)/node" geiler-musik-bot_node sh -c "pip install coverage && PYTHONWARNINGS='ignore' coverage run --parallel-mode --source=. -m unittest discover"
+mv ./node/.coverage* ./.coverage.node
+docker run --mount src=$(pwd),target=$(pwd),type=bind --env-file=sysenv.env -w="$(pwd)" geiler-musik-bot_discord sh -c "pip install coverage && PYTHONWARNINGS='ignore' coverage run --source=.,src -m unittest discover -s src/"
 coverage combine
 coverage report
 coveralls
 
-docker-compose build
 docker-compose up -d
 sleep 20s
 docker-compose logs
 bash ./discord/run_tests.sh
 docker-compose stop
+
 docker-compose rm -f
 
 #podman-compose up -d
