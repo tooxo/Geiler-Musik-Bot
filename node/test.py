4a73da0ccdfab33a7ef1ecf05f08f19e8de919d4
till <till@s.chulte.de>
2020-05-03 16:26:46 +0200

    add unittests for youtube and soundcloud

--- node/test.py
+++ node/test.py
@@ -1,7 +1,11 @@
 # pylint: skip-file
+import asyncio
+import functools
+import json
 import unittest
 
 import av_audio_source
+from node import YouTube, NotAvailableException, SoundCloud
 
 
 class NodeTest(unittest.TestCase):
@@ -53,6 +57,168 @@ class NodeTest(unittest.TestCase):
         self.assertEqual(len(source.read()), 19)
         self.assertEqual(source.volume, 0.5)
         source.cleanup()
+
+
+class NodeYouTubeTest(unittest.TestCase):
+    def test_youtube_search_basic(self):
+        y = YouTube()
+        self.assertEqual(
+            asyncio.run(
+                y.search(
+                    input_json=json.dumps(
+                        {"term": "Despacito Luis Fonsi", "service": "basic"}
+                    )
+                )
+            ),
+            "https://youtube.com/watch?v=kJQP7kiw5Fk",
+        )
+        # try again for cache
+        self.assertEqual(
+            asyncio.run(
+                y.search(
+                    input_json=json.dumps(
+                        {"term": "Despacito Luis Fonsi", "service": "basic"}
+                    )
+                )
+            ),
+            "https://youtube.com/watch?v=kJQP7kiw5Fk",
+        )
+        self.assertRaises(
+            NotAvailableException,
+            functools.partial(
+                asyncio.run,
+                (
+                    y.search(
+                        input_json=json.dumps(
+                            {
+                                "term": "sadhoiahf289ur9dsfö.c",
+                                "service": "basic",
+                            }
+                        )
+                    )
+                ),
+            ),
+        )
+
+    def test_youtube_search_music(self):
+        y = YouTube()
+        self.assertEqual(
+            asyncio.run(
+                y.search(
+                    input_json=json.dumps(
+                        {"term": "Despacito Luis Fonsi", "service": "music"}
+                    )
+                )
+            ),
+            "https://youtube.com/watch?v=kJQP7kiw5Fk",
+        )
+        # try again for cache
+        self.assertEqual(
+            asyncio.run(
+                y.search(
+                    input_json=json.dumps(
+                        {"term": "Despacito Luis Fonsi", "service": "music"}
+                    )
+                )
+            ),
+            "https://youtube.com/watch?v=kJQP7kiw5Fk",
+        )
+        self.assertRaises(
+            NotAvailableException,
+            functools.partial(
+                asyncio.run,
+                (
+                    y.search(
+                        input_json=json.dumps(
+                            {
+                                "term": "sadhoiahf289ur9dsfö.c",
+                                "service": "music",
+                            }
+                        )
+                    )
+                ),
+            ),
+        )
+
+    def test_youtube_extraction(self):
+        y = YouTube()
+        self.assertTrue(
+            asyncio.run(
+                y.youtube_extraction(
+                    "kJQP7kiw5Fk", "https://youtube.com/watch?v=kJQP7kiw5Fk"
+                )
+            )["stream"]
+        )
+        self.assertRaises(
+            NotAvailableException,
+            functools.partial(
+                asyncio.run,
+                (
+                    y.youtube_extraction(
+                        "kJQP7kiw5Fl", "https://youtube.com/watch?v=kJkr7niw5Fl"
+                    )
+                ),
+            ),
+        )
+
+    def test_playlist(self):
+        y = YouTube()
+        self.assertEqual(
+            len(
+                asyncio.run(
+                    y.extract_playlist("PLw-VjHDlEOgtl4ldJJ8Arb2WeSlAyBkJS")
+                )
+            ),
+            100,
+        )
+        self.assertEqual(
+            len(
+                asyncio.run(
+                    y.extract_playlist("PLw-VjHDlEOgtlkkkJJ8Arb2WeSlAyBkJS")
+                )
+            ),
+            0,
+        )
+
+
+class NodeSoundCloudTest(unittest.TestCase):
+    def test_search(self):
+        s = SoundCloud()
+        self.assertTrue(asyncio.run(s.search("roddy ricch the box"))["stream"],)
+        self.assertRaises(
+            NotAvailableException,
+            functools.partial(asyncio.run, (s.search("sadjaosdij20930329a"))),
+        )
+
+    def test_track(self):
+        s = SoundCloud()
+        self.assertEqual(
+            asyncio.run(
+                s.research_track("https://soundcloud.com/roddyricch/the-box")
+            )["title"],
+            "The Box",
+        )
+        self.assertRaises(
+            NotAvailableException,
+            functools.partial(
+                asyncio.run,
+                (s.research_track("https://soundcloud.com/roddyricch/the-lox")),
+            ),
+        )
+
+    def test_playlist(self):
+        s = SoundCloud()
+        self.assertEqual(
+            len(
+                asyncio.run(
+                    s.playlist(
+                        "https://soundcloud.com/user-577202417-134766123/sets"
+                        "/top-50-all-music-genres"
+                    )
+                )
+            ),
+            50,
+        )
 
 
 if __name__ == "__main__":
